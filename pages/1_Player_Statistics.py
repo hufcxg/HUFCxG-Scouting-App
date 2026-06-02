4e17b18004bf5d6d8e05296d4bbcf88a6a4783d7
hufcxg <shepherdo96@gmail.com>
2026-06-02 19:34:38 +0100

    Add files via upload

---
+++ pages/1_Player_Statistics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from _install import ensure_installed; ensure_installed()
+from hufcxg_scout.pages.player_statistics import run
+run()
+
---
+++ pages/2_Scouting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from _install import ensure_installed; ensure_installed()
+from hufcxg_scout.pages.scouting import run
+run()
+
