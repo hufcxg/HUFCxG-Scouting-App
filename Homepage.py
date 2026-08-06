965d0c75954727a74585666a8ff810577fe639c2
hufcxg <shepherdo96@gmail.com>
2026-08-06 18:17:02 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -94,7 +94,7 @@ st.markdown("""
         display: flex;
         flex-direction: column;
         align-items: center;
-        min-height: 290px;
+        min-height: 280px;
     }
     .nav-card:hover { border-color: #58a6ff; }
     .nav-card .icon {
